4963dab5c53917476eb3b8ac259220c6f289366c
Kyle Dillon <dillonkw.inbox@gmail.com>
2026-02-22 15:41:11 -0500

    Enhance comments in init_database.sql script
    Added comments to clarify the purpose and warnings of the script.

--- Scripts/init_database.sql
+++ Scripts/init_database.sql
@@ -1,12 +1,18 @@
 /*
-
-================================================
-Create Schemas
-================================================
-
+=============================================================
+Create Database and Schemas
+=============================================================
 Script Purpose:
-	This script sets up three schemas within the database: 'bronze', 'silver', and 'gold'.
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
 */
+
 
 -- Dropping schema if it already exists
 DROP SCHEMA IF EXISTS bronze CASCADE;
