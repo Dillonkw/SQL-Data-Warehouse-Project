520c922e46377924bb34e8fec089de41ac17de35
Kyle Dillon <dillonkw.inbox@gmail.com>
2026-02-25 22:07:58 -0500

    Modify crm_prd_info and crm_sales_details tables

--- Scripts/silver/ddl_silver.sql
+++ Scripts/silver/ddl_silver.sql
@@ -38,6 +38,7 @@ CREATE TABLE silver.crm_cust_info(
 -- Table: crm_prd_info
 CREATE TABLE silver.crm_prd_info(
     prd_id INT,
+	cat_id VARCHAR(50),
     prd_key VARCHAR(50),
     prd_nm VARCHAR(50),
     prd_cost INT,
@@ -52,9 +53,9 @@ CREATE TABLE silver.crm_sales_details(
     sls_ord_num VARCHAR(50),
     sls_prd_key VARCHAR(50),
     sls_cust_id INT,
-    sls_order_dt INT,
-    sls_ship_dt INT,
-    sls_due_dt INT,
+    sls_order_dt DATE,
+    sls_ship_dt DATE,
+    sls_due_dt DATE,
     sls_sales INT,
     sls_quantity INT,
     sls_price INT,
@@ -84,5 +85,4 @@ CREATE TABLE silver.erp_px_cat_g1v2(
     maintenance VARCHAR(50),
 	dwh_create_date TIMESTAMP DEFAULT NOW()
 );
-
 
